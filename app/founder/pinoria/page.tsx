2e975aff11d7f7a0d813ff11e004d6d65d24d0d0
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-19 12:54:03 +0700

    prototype(pinoria): wire presence flow into founder review page

--- app/founder/pinoria/page.tsx
+++ app/founder/pinoria/page.tsx
@@ -1,12 +1,15 @@
 import fontStyles from "../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../pinoria-vietnamese-locale";
 import { PinoriaPrototype } from "./pinoria-prototype";
+import { PresencePrototypeLayer } from "./presence-prototype-layer";
 
 export default function PinoriaPrototypePage() {
   return (
     <PinoriaVietnameseLocale>
       <div className={fontStyles.vnFont} lang="vi">
-        <PinoriaPrototype />
+        <PresencePrototypeLayer>
+          <PinoriaPrototype />
+        </PresencePrototypeLayer>
       </div>
     </PinoriaVietnameseLocale>
   );
