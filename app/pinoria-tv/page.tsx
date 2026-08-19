cc5d9fa80748dab17eb6e33aeb19605756c618d0
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-19 15:07:57 +0700

    Wire final quick choice polish into Pinoria TV

--- app/pinoria-tv/page.tsx
+++ app/pinoria-tv/page.tsx
@@ -1,12 +1,15 @@
 import fontStyles from "../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../pinoria-vietnamese-locale";
+import { ChoiceFinalPolishLayer } from "./choice-final-polish-layer";
 import { PinoriaTVPrototype } from "./tv-prototype";
 
 export default function PinoriaTVPage() {
   return (
     <PinoriaVietnameseLocale>
       <div className={fontStyles.vnFont} lang="vi">
-        <PinoriaTVPrototype />
+        <ChoiceFinalPolishLayer>
+          <PinoriaTVPrototype />
+        </ChoiceFinalPolishLayer>
       </div>
     </PinoriaVietnameseLocale>
   );
