2d114c9af9d6e8ec9c09bda50721d160e3fd3be2
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-19 15:41:38 +0700

    fix(pinoria-tv): disable crashing arrival polish layer

--- app/pinoria-tv/page.tsx
+++ app/pinoria-tv/page.tsx
@@ -1,6 +1,5 @@
 import fontStyles from "../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../pinoria-vietnamese-locale";
-import { ArrivalPolishLayer } from "./arrival-polish-layer";
 import { ChoiceFinalPolishLayer } from "./choice-final-polish-layer";
 import { PinoriaTVPrototype } from "./tv-prototype";
 
@@ -8,11 +7,9 @@ export default function PinoriaTVPage() {
   return (
     <PinoriaVietnameseLocale>
       <div className={fontStyles.vnFont} lang="vi">
-        <ArrivalPolishLayer>
-          <ChoiceFinalPolishLayer>
-            <PinoriaTVPrototype />
-          </ChoiceFinalPolishLayer>
-        </ArrivalPolishLayer>
+        <ChoiceFinalPolishLayer>
+          <PinoriaTVPrototype />
+        </ChoiceFinalPolishLayer>
       </div>
     </PinoriaVietnameseLocale>
   );
