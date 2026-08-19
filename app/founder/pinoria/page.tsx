6d848beb2aa6bcb2eafb35ce4f89e81fc21e86de
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-19 14:20:49 +0700

    Wire Live House polish layer

--- app/founder/pinoria/page.tsx
+++ app/founder/pinoria/page.tsx
@@ -1,5 +1,6 @@
 import fontStyles from "../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../pinoria-vietnamese-locale";
+import { LiveHousePolishLayer } from "./live-house-polish-layer";
 import { PinoriaPrototype } from "./pinoria-prototype";
 import { PresencePrototypeLayer } from "./presence-prototype-layer";
 
@@ -7,9 +8,11 @@ export default function PinoriaPrototypePage() {
   return (
     <PinoriaVietnameseLocale>
       <div className={fontStyles.vnFont} lang="vi">
-        <PresencePrototypeLayer>
-          <PinoriaPrototype />
-        </PresencePrototypeLayer>
+        <LiveHousePolishLayer>
+          <PresencePrototypeLayer>
+            <PinoriaPrototype />
+          </PresencePrototypeLayer>
+        </LiveHousePolishLayer>
       </div>
     </PinoriaVietnameseLocale>
   );
