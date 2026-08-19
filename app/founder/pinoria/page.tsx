c2ec16ffda299eaeffba321e2ba80bfb9d5ebe83
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-19 12:42:56 +0700

    fix(pinoria): apply Vietnamese-safe font to Founder prototype

--- app/founder/pinoria/page.tsx
+++ app/founder/pinoria/page.tsx
@@ -1,10 +1,13 @@
+import fontStyles from "../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../pinoria-vietnamese-locale";
 import { PinoriaPrototype } from "./pinoria-prototype";
 
 export default function PinoriaPrototypePage() {
   return (
     <PinoriaVietnameseLocale>
-      <PinoriaPrototype />
+      <div className={fontStyles.vnFont} lang="vi">
+        <PinoriaPrototype />
+      </div>
     </PinoriaVietnameseLocale>
   );
 }
