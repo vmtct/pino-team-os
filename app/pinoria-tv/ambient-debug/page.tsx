56c74e742d9043db89ed409119188a6451af8c04
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-21 16:48:50 +0700

    feat(pinoria-tv): mount ambient area boundary editor

--- app/pinoria-tv/ambient-debug/page.tsx
+++ app/pinoria-tv/ambient-debug/page.tsx
@@ -1,5 +1,6 @@
 import fontStyles from "../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../pinoria-vietnamese-locale";
+import { AmbientAreaBoundaryEditor } from "../ambient-area-boundary-editor";
 import { AmbientCharacterDepthController } from "../ambient-character-depth-controller";
 import { AmbientHouseEditorBootstrap } from "../ambient-house-editor-bootstrap";
 import { AmbientMidDebugToggle } from "../ambient-mid-debug-toggle";
@@ -11,6 +12,7 @@ export default function AmbientHouseDebugPage() {
         <AmbientHouseEditorBootstrap />
         <AmbientCharacterDepthController />
         <AmbientMidDebugToggle />
+        <AmbientAreaBoundaryEditor />
       </div>
     </PinoriaVietnameseLocale>
   );
