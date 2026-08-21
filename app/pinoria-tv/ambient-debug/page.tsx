34221d6d31e6061a087fd690c1372ac56aa4ef25
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-21 16:41:28 +0700

    feat(pinoria-tv): enable ambient char y-depth ordering

--- app/pinoria-tv/ambient-debug/page.tsx
+++ app/pinoria-tv/ambient-debug/page.tsx
@@ -1,5 +1,6 @@
 import fontStyles from "../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../pinoria-vietnamese-locale";
+import { AmbientCharacterDepthController } from "../ambient-character-depth-controller";
 import { AmbientHouseEditorBootstrap } from "../ambient-house-editor-bootstrap";
 import { AmbientMidDebugToggle } from "../ambient-mid-debug-toggle";
 
@@ -8,6 +9,7 @@ export default function AmbientHouseDebugPage() {
     <PinoriaVietnameseLocale>
       <div className={fontStyles.vnFont} lang="vi">
         <AmbientHouseEditorBootstrap />
+        <AmbientCharacterDepthController />
         <AmbientMidDebugToggle />
       </div>
     </PinoriaVietnameseLocale>
