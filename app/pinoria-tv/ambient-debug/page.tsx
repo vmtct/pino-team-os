2f2483fa8f1cac1dc4a4b4cefa856035d77b8e6a
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-21 15:43:08 +0700

    feat(pinoria-tv): expose MID highlight diagnostics in ambient debug

--- app/pinoria-tv/ambient-debug/page.tsx
+++ app/pinoria-tv/ambient-debug/page.tsx
@@ -1,12 +1,14 @@
 import fontStyles from "../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../pinoria-vietnamese-locale";
 import { AmbientHouseEditor } from "../ambient-house-editor";
+import { AmbientMidDebugToggle } from "../ambient-mid-debug-toggle";
 
 export default function AmbientHouseDebugPage() {
   return (
     <PinoriaVietnameseLocale>
       <div className={fontStyles.vnFont} lang="vi">
         <AmbientHouseEditor />
+        <AmbientMidDebugToggle />
       </div>
     </PinoriaVietnameseLocale>
   );
