7063b723f236058b86af1a181ea26bd8e497a582
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-24 15:51:36 +0700

    prototype: redirect legacy inventory TV route to canonical surface

--- app/pinoria-tv/inventory/page.tsx
+++ app/pinoria-tv/inventory/page.tsx
@@ -1,14 +1,5 @@
-import fontStyles from "../../pinoria-vietnamese-font.module.css";
-import { PinoriaVietnameseLocale } from "../../pinoria-vietnamese-locale";
-import { InventoryScene } from "../inventory-scene";
-import { PINORIA_SHOP_SURFACE_ID } from "../shop-types";
+import { redirect } from "next/navigation";
 
-export default function PinoriaInventoryReviewPage() {
-  return (
-    <PinoriaVietnameseLocale>
-      <main className={fontStyles.vnFont} lang="vi" style={{ position: "fixed", inset: 0, overflow: "hidden", background: "#1b1411" }}>
-        <InventoryScene surfaceId={PINORIA_SHOP_SURFACE_ID} />
-      </main>
-    </PinoriaVietnameseLocale>
-  );
+export default function LegacyPinoriaInventoryRoute() {
+  redirect("/pinoria-tv");
 }
