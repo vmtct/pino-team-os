cddadec3555d320db528743862ebae3bf954373b
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-27 19:41:18 +0700

    prototype: apply Shop polish to operational TV

--- app/pinoria-tv/page.tsx
+++ app/pinoria-tv/page.tsx
@@ -1,13 +1,14 @@
 import fontStyles from "../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../pinoria-vietnamese-locale";
 import { OperationalTvRemoteControl } from "./prototype-remote-controls";
+import shopPolish from "./shop-polish.module.css";
 import { ShopTvOverlay } from "./shop-tv-overlay";
 import { PinoriaTVPrototype } from "./tv-prototype";
 
 export default function PinoriaTVPage() {
   return (
     <PinoriaVietnameseLocale>
-      <div className={fontStyles.vnFont} lang="vi">
+      <div className={`${fontStyles.vnFont} ${shopPolish.shopPolish}`} lang="vi">
         <PinoriaTVPrototype />
         <ShopTvOverlay />
         <OperationalTvRemoteControl />
