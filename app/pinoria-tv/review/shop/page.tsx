69860c092980c3ec8e134c1a4a77eaa3862f710d
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-27 19:41:28 +0700

    prototype: apply Shop polish to review surface

--- app/pinoria-tv/review/shop/page.tsx
+++ app/pinoria-tv/review/shop/page.tsx
@@ -1,13 +1,14 @@
 import fontStyles from "../../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../../pinoria-vietnamese-locale";
 import { PinoriaShopRemoteControl } from "../../prototype-remote-controls";
+import shopPolish from "../../shop-polish.module.css";
 import { ShopScene } from "../../shop-scene";
 import { PINORIA_SHOP_SURFACE_ID } from "../../shop-types";
 
 export default function PinoriaShopReviewPage() {
   return (
     <PinoriaVietnameseLocale>
-      <main className={fontStyles.vnFont} lang="vi" style={{ position: "fixed", inset: 0, overflow: "hidden", background: "#1b1411" }}>
+      <main className={`${fontStyles.vnFont} ${shopPolish.shopPolish}`} lang="vi" style={{ position: "fixed", inset: 0, overflow: "hidden", background: "#1b1411" }}>
         <ShopScene surfaceId={PINORIA_SHOP_SURFACE_ID} />
         <PinoriaShopRemoteControl />
       </main>
