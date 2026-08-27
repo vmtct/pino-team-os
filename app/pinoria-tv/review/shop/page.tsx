9be3c14d9c92bf575677192eeb2f2470476f4afb
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-27 19:13:11 +0700

    prototype: mount Shop review remote

--- app/pinoria-tv/review/shop/page.tsx
+++ app/pinoria-tv/review/shop/page.tsx
@@ -1,5 +1,6 @@
 import fontStyles from "../../../pinoria-vietnamese-font.module.css";
 import { PinoriaVietnameseLocale } from "../../../pinoria-vietnamese-locale";
+import { PinoriaShopRemoteControl } from "../../prototype-remote-controls";
 import { ShopScene } from "../../shop-scene";
 import { PINORIA_SHOP_SURFACE_ID } from "../../shop-types";
 
@@ -8,6 +9,7 @@ export default function PinoriaShopReviewPage() {
     <PinoriaVietnameseLocale>
       <main className={fontStyles.vnFont} lang="vi" style={{ position: "fixed", inset: 0, overflow: "hidden", background: "#1b1411" }}>
         <ShopScene surfaceId={PINORIA_SHOP_SURFACE_ID} />
+        <PinoriaShopRemoteControl />
       </main>
     </PinoriaVietnameseLocale>
   );
